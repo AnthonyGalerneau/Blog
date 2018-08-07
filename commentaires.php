4033f821b9cb4d0f73b71c2762e1270fa7657962
Anthony Galerneau <contact@anthony-galerneau.fr>
2018-08-07 12:50:01 +0200

    Ajout des commentaires sur page commentaire

--- commentaires.php
+++ commentaires.php
@@ -10,7 +10,6 @@
 
 <?php 
 $bdd = new PDO('mysql:host=localhost;dbname=Blog', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
 $reponse = $bdd->prepare('SELECT id, titre, contenu, date_creation FROM billets WHERE id = ?'); 
 $reponse->execute(array($_GET['billet']));
 $donnees = $reponse->fetch();
@@ -28,5 +27,29 @@ $donnees = $reponse->fetch();
 	</p>
 </div>
 <h2>Commentaires :</h2>
+<?php
+$reponse->closeCursor();
+$bdd = new PDO('mysql:host=localhost;dbname=Blog', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$reponse = $bdd->prepare('SELECT auteur, commentaire, date_commentaire FROM commentaires WHERE id_billet = ?'); 
+$reponse->execute(array($_GET['billet']));
+while ($donnees = $reponse->fetch())
+{
+?>
+<div id="espace_commentaire">
+	<h3>
+	<?php
+		echo htmlspecialchars($donnees['auteur'] . ' - ' . $donnees['date_commentaire']);
+	?>
+	</h3>
+	<p>
+	<?php
+		echo htmlspecialchars($donnees['commentaire']);
+	?>
+	</p>
+</div>
+<?php
+}
+$reponse->closeCursor();
+?>
 </body>
 </html>
