ea68a27e6ba1ae1b4357f2ca369bd334a903afbb
Anthony Galerneau <contact@anthony-galerneau.fr>
2018-08-07 15:29:27 +0200

    Ajout 'isset' à commentaires

--- commentaires.php
+++ commentaires.php
@@ -11,46 +11,53 @@
 
 <?php 
 $bdd = new PDO('mysql:host=localhost;dbname=Blog', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$reponse = $bdd->prepare('SELECT id, titre, contenu, date_creation FROM billets WHERE id = ?'); 
-$reponse->execute(array($_GET['billet']));
-$donnees = $reponse->fetch();
-?>
-<div id="billet">
-	<h3>
-	<?php
-		echo htmlspecialchars($donnees['titre'] . ' - ' . $donnees['date_creation']);
-	?>
-	</h3>
-	<p>
-	<?php
-		echo htmlspecialchars($donnees['contenu']);
-	?>
-	</p>
-</div>
-<h2>Commentaires :</h2>
-<?php
-$reponse->closeCursor();
-$bdd = new PDO('mysql:host=localhost;dbname=Blog', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$reponse = $bdd->prepare('SELECT auteur, commentaire, date_commentaire FROM commentaires WHERE id_billet = ?'); 
-$reponse->execute(array($_GET['billet']));
-while ($donnees = $reponse->fetch())
+if (isset($_GET['billet']))
 {
-?>
-<div id="espace_commentaire">
-	<h3>
-	<?php
-		echo htmlspecialchars($donnees['auteur'] . ' - ' . $donnees['date_commentaire']);
+	$reponse = $bdd->prepare('SELECT id, titre, contenu, date_creation FROM billets WHERE id = ?'); 
+	$reponse->execute(array($_GET['billet']));
+	$donnees = $reponse->fetch();
 	?>
-	</h3>
-	<p>
+	<div id="billet">
+		<h3>
+		<?php
+			echo htmlspecialchars($donnees['titre'] . ' - ' . $donnees['date_creation']);
+		?>
+		</h3>
+		<p>
+		<?php
+			echo htmlspecialchars($donnees['contenu']);
+		?>
+		</p>
+	</div>
+	<h2>Commentaires :</h2>
 	<?php
-		echo htmlspecialchars($donnees['commentaire']);
+	$reponse->closeCursor();
+	$bdd = new PDO('mysql:host=localhost;dbname=Blog', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$reponse = $bdd->prepare('SELECT auteur, commentaire, date_commentaire FROM commentaires WHERE id_billet = ?'); 
+	$reponse->execute(array($_GET['billet']));
+	while ($donnees = $reponse->fetch())
+	{
 	?>
-	</p>
-</div>
-<?php
+	<div id="espace_commentaire">
+		<h3>
+		<?php
+			echo htmlspecialchars($donnees['auteur'] . ' - ' . $donnees['date_commentaire']);
+		?>
+		</h3>
+		<p>
+		<?php
+			echo htmlspecialchars($donnees['commentaire']);
+		?>
+		</p>
+	</div>
+	<?php
+	}
+	$reponse->closeCursor();
 }
-$reponse->closeCursor();
+else
+{
+	echo 'Page introuvable !';
+}
 ?>
 </body>
 </html>
