abf885747bc3775dad94cd3e34b7381ecff15ce2
Anthony Galerneau <contact@anthony-galerneau.fr>
2018-08-07 10:59:06 +0200

    Affichage des billets sur index.php

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
 <body>
 	<h1>Mon super blog !</h1>
 	<p>Derniers billets du blog</p>
+<?php 
+$bdd = new PDO('mysql:host=localhost;dbname=Blog', 'root', 'root');
+$reponse = $bdd->query('SELECT * FROM billets'); 
+while ($donnees = $reponse->fetch())
+{
+	echo '<p>' . $donnees['titre'] . ' ' . $donnees['date_creation'] . '</p>';
+	echo '<p>' . $donnees['contenu'] . '</p>';
+}
+
+
+?>
 
 </body>
 </html>
