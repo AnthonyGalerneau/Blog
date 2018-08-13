bf676ca4ebf597a3d8934424dfc5eb841fa6258e
Anthony Galerneau <contact@anthony-galerneau.fr>
2018-08-13 16:16:51 +0200

    isolation de l'accès aux données dans modele.php

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 
 <?php 
+require('modele.php');
 
-//Récupération des données
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=Blog', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
-
-$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%i\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+$req = getBillets();
 
 require('affichageAccueil.php');
-
 ?>
+
+
