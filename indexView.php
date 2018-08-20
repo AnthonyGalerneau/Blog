5694bf254bca5d20aecfe97132c621438f9d2bbb
Anthony Galerneau <contact@anthony-galerneau.fr>
2018-08-20 14:34:11 +0200

    création template

--- indexView.php
+++ indexView.php
@@ -1,35 +1,30 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-        <link href="style.css" rel="stylesheet" /> 
-    </head>
+<?php $title = 'Mon blog'; ?>
+
+<?php ob_start(); ?>
+<h1>Mon super blog !</h1>
+<p>Derniers billets du blog :</p>
+
+
+<?php
+while ($data = $posts->fetch())
+{
+?>
+    <div class="post">
+        <h3>
+            <?= htmlspecialchars($data['title']) ?>
+            le <?= $data['creation_date_fr'] ?>
+        </h3>
         
-    <body>
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
- 
-        
-        <?php
-        while ($data = $posts->fetch())
-        {
-        ?>
-            <div class="post">
-                <h3>
-                    <?= htmlspecialchars($data['title']) ?>
-                    le <?= $data['creation_date_fr'] ?>
-                </h3>
-                
-                <p>
-                    <?= nl2br(htmlspecialchars($data['content'])) ?>
-                    <br />
-                    <a href="post.php?id=<?= $data['id'] ?>">Commentaires</a>
-                </p>
-            </div>
-        <?php
-        }
-        $posts->closeCursor();
-        ?>
-    </body>
-</html>
+        <p>
+            <?= nl2br(htmlspecialchars($data['content'])) ?>
+            <br />
+            <a href="post.php?id=<?= $data['id'] ?>">Commentaires</a>
+        </p>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
