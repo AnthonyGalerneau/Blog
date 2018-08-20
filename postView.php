cf16b6cd34721015f5be4672797430b2c2b1bb95
AnthonyGalerneau <42116803+AnthonyGalerneau@users.noreply.github.com>
2018-08-20 15:10:28 +0200

    Update postView.php

--- postView.php
+++ postView.php
@@ -1,46 +1,41 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-        <link href="style.css" rel="stylesheet" /> 
-    </head>
-        
-    <body>
-        <h1>Mon super blog !</h1>
-        <p><a href="index.php">Retour à la liste des billets</a></p>
+<?php $title = htmlspecialchars($post['title']); ?>
 
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($post['title']) ?>
-                <em>le <?= $post['creation_date_fr'] ?></em>
-            </h3>
-            
-            <p>
-                <?= nl2br(htmlspecialchars($post['content'])) ?>
-            </p>
-        </div>
+<?php ob_start(); ?>
+<h1>Mon super blog !</h1>
+<p><a href="index.php">Retour à la liste des billets</a></p>
 
-        <h3>Commentaires</h3>
+<div class="news">
+    <h3>
+        <?= htmlspecialchars($post['title']) ?>
+        <em>le <?= $post['creation_date_fr'] ?></em>
+    </h3>
+    
+    <p>
+        <?= nl2br(htmlspecialchars($post['content'])) ?>
+    </p>
+</div>
 
-        <?php
-        while ($comment = $comments->fetch())
-        {
-        ?>
-            <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
-            <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-        <?php
-        }
-        ?>
+<h3>Commentaires</h3>
 
-        <!--<div id="formulaire_commentaire">
-		<form action="comments_post.php" method="POST">
-			<p><label for="author"> Pseudo :<input type="text" name="author" id="author" /></label></p>
-			<p><label for="comment">  Commentaire : <textarea name="comment" rows="4" cols="30" id="comment"></textarea></label></p>
-			<p><input type="submit" /></p> 
-			<input type="hidden" for="comment_date" name="comment_date" id="comment_date value="<?php //echo '' . time();?>" /> 
-			<input for="id_post" type="hidden" name="id_post" id="id_post" value="<?php //echo $_GET['post']; ?>" />  
-	    </form>-->
-	</div>
-    </body>
-</html>
+<?php
+while ($comment = $comments->fetch())
+{
+?>
+    <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
+    <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+<?php
+}
+?>
+
+<!--<div id="formulaire_commentaire">
+<form action="comments_post.php" method="POST">
+	<p><label for="author"> Pseudo :<input type="text" name="author" id="author" /></label></p>
+	<p><label for="comment">  Commentaire : <textarea name="comment" rows="4" cols="30" id="comment"></textarea></label></p>
+	<p><input type="submit" /></p> 
+	<input type="hidden" for="comment_date" name="comment_date" id="comment_date value="<?php //echo '' . time();?>" /> 
+	<input for="id_post" type="hidden" name="id_post" id="id_post" value="<?php //echo $_GET['post']; ?>" />  
+</form>-->
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
